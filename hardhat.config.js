2da9b526147a0ed8dce0cb360dda60ec5e03262f
HighSpeed1380 <rabconnection1380@gmail.com>
2023-05-25 03:06:11 -0700

    update

---
+++ hardhat.config.js
@@ -0,0 +1,28 @@
+require("@nomicfoundation/hardhat-toolbox");
+// Import and configure dotenv
+require("dotenv").config();
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+      console.log(account.address);
+  }
+});
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    goerli: {
+      url: process.env.STAGING_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      chainId: 1,
+      url: process.env.PROD_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
